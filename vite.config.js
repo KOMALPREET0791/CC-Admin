9dcc270c041804a3dd03b743f3f63b885ea07255
komalpreet <komalpreet0791.be21@chitkara.edu.in>
2025-05-09 13:32:35 +0530

    z

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-    allowedHosts: ['cc-frontend-rsp0.onrender.com']
+    allowedHosts: ['cc-admin.onrender.com']
 
   }
 });
